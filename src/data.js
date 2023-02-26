3b51f50c119c86361661791d70ed07afdca70cc6
Will <avatec2312@gmail.com>
2023-02-26 02:45:03 -0500

    Starting game data in own module

---
+++ src/data.js
@@ -0,0 +1,21 @@
+import Player from './factories/player.js';
+import gameBoard from './factories/gameboard.js';
+import ship from './factories/ship.js';
+
+export default (function () {
+	const player1 = new Player();
+	const computer = new Player('computer');
+	const isGameOver = false;
+	const playerBoard = gameBoard();
+	const enemyBoard = gameBoard();
+	const ship3 = ship(3);
+	ship.isVertical = true;
+	return {
+		player1,
+		computer,
+		isGameOver,
+		playerBoard,
+		enemyBoard,
+		ship3,
+	};
+})();
