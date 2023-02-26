4d057a228dbfee581d91a31dfefa2b66084f7751
Will <avatec2312@gmail.com>
2023-02-25 22:54:05 -0500

    Working factories

---
+++ src/factories/ship.js
@@ -0,0 +1,26 @@
+export default function (size = 1) {
+  const length = size;
+  let sunked = false;
+  let hits = 0;
+  const isVertical = false;
+
+  function hit(damage = 1) {
+    if (hits < length) {
+      hits += damage;
+    }
+    return hits;
+  }
+  function getSize() {
+    return length;
+  }
+  function isSunk() {
+    sunked = hits >= length;
+    return { sunked, hits };
+  }
+  return {
+    isSunk,
+    hit,
+    getSize,
+    isVertical,
+  };
+}
