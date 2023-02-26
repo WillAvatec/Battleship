84df357eb84377d1de3fb9a3134835c7aecc42eb
Will <avatec2312@gmail.com>
2023-02-26 02:52:03 -0500

    Adding svg-inline-loader to set gitLogo

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,24 @@ const path = require('path');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-  entry: './src/controller.js',
-  mode: 'development',
-  plugins: [new ESLintPlugin()],
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      // Additional configuration to handle *.css files
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
+	entry: './src/controller.js',
+	mode: 'development',
+	plugins: [new ESLintPlugin()],
+	output: {
+		publicPath: '',
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.svg$/g,
+				loader: 'svg-inline-loader',
+			},
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+		],
+	},
 };
