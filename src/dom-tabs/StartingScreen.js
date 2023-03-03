f5a9f9479218d89158671fdb9d8182174110928d
Will <avatec2312@gmail.com>
2023-03-03 01:05:12 -0500

    Rework eventListener and removing title from modal

--- src/dom-tabs/StartingScreen.js
+++ src/dom-tabs/StartingScreen.js
@@ -1,21 +1,24 @@
-import domHelper from './assistance/domHelper.js';
+import {domHelper, clearChildNodes} from './assistance/domHelper.js';
 
 const inputName = domHelper('input', {class: 'nameInput', type: 'text', placeholder: 'Please enter your name'}, ['Player 1']);
 const playButton = domHelper('button', {class: 'playBtn'}, ['Start Game']);
 const form = domHelper('form', {}, [inputName, playButton]);
-const title = domHelper('h1', {id: 'title'}, ['BattleShip']);
+// Const title = domHelper('h1', {id: 'title'}, ['BattleShip']);
 // This is what is going to be presented first
-const modalDiv = domHelper('div', {class: 'modal'}, [title, form]);
+const modalDiv = domHelper('div', {class: 'modal'}, [form]);
 
 // Now here's the eventListener Logic
 // - Prevent reloading window;
 // - Call data.js, passing the name in the input
 // - Save the data created in a variable
+const body = document.querySelector('body');
 
-export default function ({parentNode, callback, storeDataIn}) {
-	playButton.addEventListener('click', e => {
+export default function (callback) {
+	form.addEventListener('submit', e => {
+		console.log('submitted');
 		e.preventDefault();
-		storeDataIn = callback(inputName.value);
+		callback(inputName.value);
+		clearChildNodes(modalDiv);
 	});
-	parentNode.appendChild(modalDiv);
+	body.appendChild(modalDiv);
 }
