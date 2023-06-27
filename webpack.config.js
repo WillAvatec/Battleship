c886a40bf4abdc92308d9745a73cc0b74a6ca150
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-06-27 16:27:52 -0500

    Install webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,29 @@
-const path = require('path');
-const ESLintPlugin = require('eslint-webpack-plugin');
+const path = require("path");
+const ESLintPlugin = require("eslint-webpack-plugin");
 
 module.exports = {
-	entry: './src/controller.js',
-	mode: 'development',
-	plugins: [new ESLintPlugin()],
-	output: {
-		publicPath: '',
-		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist'),
-	},
-	module: {
-		rules: [
-			{
-				test: /\.svg$/g,
-				loader: 'svg-inline-loader',
-			},
-			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
-			},
-		],
-	},
+  devServer: {
+    static: "./dist",
+  },
+  entry: "./src/controller.js",
+  mode: "development",
+  plugins: [new ESLintPlugin()],
+  devtool: "inline-source-map",
+  output: {
+    publicPath: "",
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svg$/g,
+        loader: "svg-inline-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
