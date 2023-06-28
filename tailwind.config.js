98f199723ef6cb9f93cc070255ce299fc1ff61b4
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-06-27 22:13:00 -0500

    Remove animate dependency
    It crashes with Tailwind and PostCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
+//eslint
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/*.{html,js}", "./src/**/*.js", "./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "fade-in": "fadeIn .5s ease-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": {
+            opacity: 0,
+            transform: "translate3d(0, -20%, 0)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate3d(0, 0, 0)",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
