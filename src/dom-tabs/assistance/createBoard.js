546a9ca2cd9d855058b9d2d2a1bdf26363e3417c
WillAvatec <115133403+WillAvatec@users.noreply.github.com>
2023-07-02 22:47:33 -0500

    Night theme (#1)
    * Install and config tailwindcss
    
    * Install webpack dev server
    
    * Install postcss loader and config with webpack
    
    * Remove animate dependency
    
    It crashes with Tailwind and PostCSS
    
    * Add customized breakpoint
    
    * Style: Fucsia neon
    
    * Remove unused screen sizes
    
    * Convert styles to classes
    
    * Append disable div to board
    
    * Change form width to 11/12 of screen
    
    * chore: Decrease wave z-index to avoid overlapping
    
    * Remove unused svg-inline-loader
    
    * feat: Links displays svg and opens new tab
    
    * feat: add new line of marke at bottom of gameGrid
    
    * Move to dist folder
    
    * Add new script to deploy on script run

--- src/dom-tabs/assistance/createBoard.js
+++ src/dom-tabs/assistance/createBoard.js
@@ -1,35 +1,46 @@
 function createMarker(text) {
-	const marker = document.createElement('div');
-	marker.className = 'marker';
-	marker.textContent = text;
-	return marker;
+  const marker = document.createElement("div");
+  marker.setAttribute(
+    "class",
+    "marker -left-5 text-white absolute flex text-sm"
+  );
+  marker.textContent = text;
+  return marker;
 }
 
 function createRow(rowNumber) {
-	const row = document.createElement('div');
-	row.setAttribute('class', 'rows');
-	row.dataset.row = rowNumber;
-	return row;
+  const row = document.createElement("div");
+  row.setAttribute("class", "rows w-full flex flex-1");
+  row.dataset.row = rowNumber;
+  return row;
 }
 
 function createCell(columnNumber) {
-	const cell = document.createElement('div');
-	cell.setAttribute('class', 'cells');
-	cell.dataset.column = columnNumber;
-	return cell;
+  const cell = document.createElement("div");
+  cell.setAttribute(
+    "class",
+    "cells inline-flex text-xl border border-opacity-75 border-pink-600"
+  );
+  cell.dataset.column = columnNumber;
+  return cell;
 }
 
 // This renders a board in the DOM
 export default function renderBoard(size, parent) {
-	for (let i = 0; i < size; i += 1) {
-		const row = createRow(i);
-		row.appendChild(createMarker(i));
-		for (let j = 0; j < size; j += 1) {
-			const cell = createCell(j);
-			row.appendChild(cell);
-		}
+  for (let i = 0; i < size; i += 1) {
+    const row = createRow(i);
+    row.appendChild(createMarker(i));
+    for (let j = 0; j < size; j += 1) {
+      const cell = createCell(j);
+      row.appendChild(cell);
+      if (i === size - 1) {
+        const bottomMarker = createMarker(j);
+        bottomMarker.classList.add("-bottom-6");
+        bottomMarker.classList.remove("-left-5");
+        cell.appendChild(bottomMarker);
+      }
+    }
 
-		parent.appendChild(row);
-	}
+    parent.appendChild(row);
+  }
 }
-
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,30 @@
-const path = require('path');
-const ESLintPlugin = require('eslint-webpack-plugin');
+const path = require("path");
+const ESLintPlugin = require("eslint-webpack-plugin");
 
 module.exports = {
-	entry: './src/controller.js',
-	mode: 'development',
-	plugins: [new ESLintPlugin()],
-	output: {
-		publicPath: '',
-		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist'),
-	},
-	module: {
-		rules: [
-			{
-				test: /\.svg$/g,
-				loader: 'svg-inline-loader',
-			},
-			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
-			},
-		],
-	},
+  devServer: {
+    static: "./dist",
+  },
+  entry: "./src/controller.js",
+  mode: "development",
+  plugins: [new ESLintPlugin()],
+  devtool: "inline-source-map",
+  output: {
+    publicPath: "",
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
 };
