162cf4e00dc74048d3ef6fe25d2b82aa0397fe2a
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-07-03 16:15:18 -0500

    Modified version with linter

--- src/data.js
+++ src/data.js
@@ -1,17 +1,16 @@
-import Player from './factories/player.js';
-import gameBoard from './factories/gameboard.js';
-import ship from './factories/ship.js';
-import 'animate.css';
+import Player from "./factories/player.js";
+import gameBoard from "./factories/gameboard.js";
+import ship from "./factories/ship.js";
 
 export default function (userName) {
-	return {
-		player1: new Player(userName),
-		computer: new Player('computer'),
-		gameStart: true,
-		isGameOver: false,
-		playerBoard: gameBoard(),
-		enemyBoard: gameBoard(),
-		playerShips: [ship(4), ship(4), ship(3), ship(3), ship(2)],
-		computerShips: [ship(4), ship(4), ship(3), ship(3), ship(2)],
-	};
+  return {
+    player1: new Player(userName),
+    computer: new Player("computer"),
+    gameStart: true,
+    isGameOver: false,
+    playerBoard: gameBoard(),
+    enemyBoard: gameBoard(),
+    playerShips: [ship(4), ship(4), ship(3), ship(3), ship(2)],
+    computerShips: [ship(4), ship(4), ship(3), ship(3), ship(2)],
+  };
 }
--- src/dom-tabs/assistance/placeShipController.js
+++ src/dom-tabs/assistance/placeShipController.js
@@ -5,7 +5,7 @@
             - Only render placed ships in playerBoard
 */
 
-import {domHelper} from './domHelper';
+import { domHelper } from "./domHelper";
 
 /*
     -To accomplish that, is needed:
@@ -25,64 +25,72 @@ let currentShip = 0;
 const savedPositions = [];
 
 const markCurrentShip = (shipSelector, container) => {
-	if (!shipSelector.dataset.shiporder) {
-		return;
-	}
+  if (!shipSelector.dataset.shiporder) {
+    return;
+  }
 
-	currentShip = Number(shipSelector.dataset.shiporder);
-	container.querySelectorAll('.ship-selector').forEach(selector => {
-		selector.classList.remove('selected');
-	});
-	shipSelector.classList.add('selected');
+  currentShip = Number(shipSelector.dataset.shiporder);
+  container.querySelectorAll(".ship-selector").forEach((selector) => {
+    selector.classList.remove("selected");
+  });
+  shipSelector.classList.add("selected");
 };
 
 // If is the player setting then receive coords from dom
 // If is the pc setting then invoke random set boats
 function saveCoords(coords) {
-	console.log(coords);
-	const object = savedPositions.find(obj => obj.ref === currentShip);
-	if (object) {
-		object.coords = coords;
-		console.log(savedPositions);
-		return;
-	}
+  console.log(coords);
+  const object = savedPositions.find((obj) => obj.ref === currentShip);
+  if (object) {
+    object.coords = coords;
+    console.log(savedPositions);
+    return;
+  }
 
-	savedPositions.push({coords, ref: currentShip});
-	console.log(savedPositions);
+  savedPositions.push({ coords, ref: currentShip });
+  console.log(savedPositions);
 }
 
 function getShip(ships) {
-	return ships[currentShip];
+  return ships[currentShip];
 }
 
 function getCoords(cell) {
-	return [Number(cell.parentNode.dataset.row),
-		Number(cell.dataset.column)];
+  return [Number(cell.parentNode.dataset.row), Number(cell.dataset.column)];
 }
 
 const setShips = (cell, [playerBoard, playerShips]) => {
-	if (playerBoard.isAvailable(getCoords(cell), getShip(playerShips))) {
-		saveCoords(getCoords(cell));
-	}
+  if (playerBoard.isAvailable(getCoords(cell), getShip(playerShips))) {
+    saveCoords(getCoords(cell));
+  }
 
-	return savedPositions;
+  return savedPositions;
 };
 
 const renderShip = (ship, container) => {
-	const gridContainer = domHelper('div', {class: 'ship-on-grid'}, []);
-	gridContainer.dataset.vertical = getShip(ship).isVertical;
-	for (let i = 0; i < getShip(ship).getSize(); i++) {
-		const shipCell = domHelper('div', {class: 'ship-cells'}, []);
-		gridContainer.appendChild(shipCell);
-	}
+  const gridContainer = domHelper("div", { class: "ship-on-grid" }, []);
+  gridContainer.dataset.vertical = getShip(ship).isVertical;
+  for (let i = 0; i < getShip(ship).getSize(); i++) {
+    const shipCell = domHelper(
+      "div",
+      {
+        class:
+          "ship-cells w-6 h-6 md:w-8 md:h-8 bg-indigo-50 relative pointer-events-none",
+      },
+      []
+    );
+    gridContainer.appendChild(shipCell);
+  }
 
-	container.appendChild(gridContainer);
+  container.appendChild(gridContainer);
 };
+
+// This add the ships into the playerBoard counterpart
 
 const putInGameBoard = ([playerBoard, ships]) => {
-	savedPositions.forEach(pos => {
-		playerBoard.setShip(ships[pos.ref], pos.coords);
-	});
+  savedPositions.forEach((pos) => {
+    playerBoard.setShip(ships[pos.ref], pos.coords);
+  });
 };
 
-export {markCurrentShip, setShips, renderShip, putInGameBoard};
+export { markCurrentShip, setShips, renderShip, putInGameBoard };
